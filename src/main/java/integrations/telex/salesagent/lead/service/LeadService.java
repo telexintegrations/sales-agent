c833c275b7d704af6f07d3c1e8be5f9a547359b3
tachtwitch <gravitox22@gmail.com>
2025-03-24 12:11:30 +0100

    fix: fixes bug in ChatServices that breaks agent flow #2

--- src/main/java/integrations/telex/salesagent/lead/service/LeadService.java
+++ src/main/java/integrations/telex/salesagent/lead/service/LeadService.java
@@ -222,7 +222,7 @@ public class LeadService {
             Response response = okHttpClient.newCall(request).execute();
             byte[] responseBody = response.body().bytes();
             JsonNode jsonNode = objectMapper.readTree(responseBody);
-            return ResponseEntity.ok(jsonNode.get("data").get("calls"));
+            return ResponseEntity.ok(jsonNode.get("data"));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
