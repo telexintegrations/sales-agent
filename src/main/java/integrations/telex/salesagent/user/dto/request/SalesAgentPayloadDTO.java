395e96db058f7c42342aca30bcaa7538332a7143
aniebietafia <aniebietafia87@gmail.com>
2025-03-26 15:35:58 +0100

    fix: Extracting information from new Telex Payload.

--- src/main/java/integrations/telex/salesagent/user/dto/request/SalesAgentPayloadDTO.java
+++ src/main/java/integrations/telex/salesagent/user/dto/request/SalesAgentPayloadDTO.java
@@ -2,13 +2,18 @@ package integrations.telex.salesagent.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 
+import java.lang.reflect.MalformedParameterizedTypeException;
 import java.util.List;
+import java.util.Map;
 
 public record SalesAgentPayloadDTO(
         @NotBlank(message = "channel ID is required")
         String channel_id,
         @NotBlank(message = "Message is required")
         String message,
+        String thread_id,
+        String org_id,
+        Map<String, String> auth_settings,
         List<Setting> settings
 ) {
 }
